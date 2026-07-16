9770d282d8c26d18e85f5aa1e9b38cff1ea3c139
Aruna Giri <ag8876@nyu.edu>
2026-07-16 02:07:21 -0400

    fix: update tailwind darkMode to string format

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  darkMode: ["class"], // Required for manual dark mode toggle
+  darkMode: "class", // Required for manual dark mode toggle
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
